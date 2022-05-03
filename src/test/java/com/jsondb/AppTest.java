fdbdce4186cb59afccb868ff2883fb82d05591ba
AlexStan0 <stan_alexandru06@yahoo.com>
2022-05-03 12:43:38 -0400

    have to test class better

--- src/test/java/com/jsondb/AppTest.java
+++ src/test/java/com/jsondb/AppTest.java
@@ -7,12 +7,17 @@ public class AppTest {
     
     public static void main(String[] args) throws Exception {
 
-        String path = "app.json";
+        String path = "./app.json";
 
         final JsonDB DB = new JsonDB(path);
 
-        DB.delete("Alex");
+        DB.setObj("Student 1", "name", "Alex", "age", 16, "gender", "male");
 
+        Object age = DB.get("Student 1", "age");
+        System.out.println((long)age == 16);
+
+        Boolean has = DB.has("Student 1", "name");
+        System.out.println(has);
     }   
 
 
